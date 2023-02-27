9718fe2ee1f8750ece7947d32fe5974a5f8f6758
Abhishek Thakur <abhisheksep2003@gmail.com>
2023-02-27 14:20:45 +0530

    formated code in order

--- Array/Assignment2.cpp
+++ Array/Assignment2.cpp
@@ -1,53 +1,54 @@
-//Multiply two Matrices
+// Multiply two Matrices
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout<<"enter total rows you want : "<<endl;
-    int totalRow ;
-    cin>>totalRow;
+int main()
+{
+    cout << "enter total rows you want : " << endl;
+    int totalRow;
+    cin >> totalRow;
 
-    cout<<"enter total Col you want : "<<endl;
-    int totalCol ;
-    cin>>totalCol;
+    cout << "enter total Col you want : " << endl;
+    int totalCol;
+    cin >> totalCol;
 
     int Array1[totalRow][totalCol];
 
-    for(int row = 0; row<totalRow ; row++){
-        for(int col =0; col<totalCol ; col++){
-            cin>>Array1[row][col];
+    for (int row = 0; row < totalRow; row++)
+    {
+        for (int col = 0; col < totalCol; col++)
+        {
+            cin >> Array1[row][col];
         }
-        cout<<endl;
+        cout << endl;
     }
 
-
     int Array2[totalRow][totalCol];
-     for(int row = 0; row<totalRow ; row++){
-        for(int col =0; col<totalCol ; col++){
-            cin>>Array2[row][col];
+    for (int row = 0; row < totalRow; row++)
+    {
+        for (int col = 0; col < totalCol; col++)
+        {
+            cin >> Array2[row][col];
         }
-        cout<<endl; 
+        cout << endl;
     }
 
     int Array3[totalRow][totalCol];
-     for(int row = 0; row<totalRow ; row++){
-        for(int col =0; col<totalCol ; col++){
-           Array3[row][col]= Array1[row][col] *  Array2[row][col];
+    for (int row = 0; row < totalRow; row++)
+    {
+        for (int col = 0; col < totalCol; col++)
+        {
+            Array3[row][col] = Array1[row][col] * Array2[row][col];
         }
-        cout<<endl;
+        cout << endl;
     }
 
-    for(int row = 0; row<totalRow ; row++){
-        for(int col =0; col<totalCol ; col++){
-            cout<<Array3[row][col]<<" ";
+    for (int row = 0; row < totalRow; row++)
+    {
+        for (int col = 0; col < totalCol; col++)
+        {
+            cout << Array3[row][col] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
-
-
- 
- 
-
-
 }
-
